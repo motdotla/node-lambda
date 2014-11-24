1d8b7ffc518c57254c0dfbeb7920b5dac5d68611
motdotla <mot@mot.la>
2014-11-24 01:49:06 -0500

    ignore the event.json file

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.1.3" ); 
+    result.version.should.eql( "0.1.4" ); 
   } );
 
   describe( "_zipfileTmpPath", function( ) {
