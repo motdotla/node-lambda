c51559531f3de190f66edaa5ac668094f25dd3f7
motdotla <mot@mot.la>
2014-11-23 20:42:35 -0500

    Adds travis

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.0.15" ); 
+    result.version.should.eql( "0.0.16" ); 
   } );
 
   describe( "_zipfileTmpPath", function( ) {
