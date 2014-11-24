275474c517525cb7f433b71603fe9460032fb184
motdotla <mot@mot.la>
2014-11-24 01:28:33 -0500

    Version bump - 0.1.1

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.1.0" ); 
+    result.version.should.eql( "0.1.1" ); 
   } );
 
   describe( "_zipfileTmpPath", function( ) {
