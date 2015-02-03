e9911486c8ccfb3b822cd168a2240b53a75a89ac
Yamil Asusta <yamil@rebelmail.com>
2015-02-03 15:18:46 -0400

    FunctionName convention

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.2.4" ); 
+    result.version.should.eql( "0.2.5" ); 
   } );
 
   describe( "_zipfileTmpPath", function( ) {
