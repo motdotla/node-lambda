841d1e5a363489aaea43b9875744a3e30c4e8f2a
Yamil Asusta <yamil@rebelmail.com>
2015-02-03 14:59:31 -0400

    Version bump

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.2.3" ); 
+    result.version.should.eql( "0.2.4" ); 
   } );
 
   describe( "_zipfileTmpPath", function( ) {
