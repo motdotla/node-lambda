c15da1418753cfb39c14cd1460399bb8227baa68
motdotla <mot@mot.la>
2015-02-11 16:04:08 -0500

    Fix /tmp/ error

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.3.0" ); 
+    result.version.should.eql( "0.3.1" ); 
   } );
   
   describe( "_params", function( ) {
