fc882e6936d2ebd0a4123399de17f394b83c0573
motdotla <mot@mot.la>
2015-02-11 16:21:43 -0500

    Fix. Version bump 0.3.3

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.3.2" ); 
+    result.version.should.eql( "0.3.3" ); 
   } );
   
   describe( "_params", function( ) {
