d493c600d1d19a3f594580236c41b9978acb6c14
motdotla <mot@mot.la>
2015-02-11 16:16:48 -0500

    Fix

--- test/main.js
+++ test/main.js
@@ -28,7 +28,7 @@ describe( "dotenv", function( ) {
   } );
 
   it( "version should be set", function( ) {
-    result.version.should.eql( "0.3.1" ); 
+    result.version.should.eql( "0.3.2" ); 
   } );
   
   describe( "_params", function( ) {
