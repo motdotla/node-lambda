8fe53dd61a55d637d2815d802ce8ee6a629d4762
Chase Sillevis <chase@pebble.com>
2016-05-12 12:09:00 +0200

    Bump 0.8.3

--- test/main.js
+++ test/main.js
@@ -34,7 +34,7 @@ describe('node-lambda', function () {
   });
 
   it('version should be set', function () {
-    assert.equal(lambda.version, '0.8.2');
+    assert.equal(lambda.version, '0.8.3');
   });
 
   describe('_params', function () {
