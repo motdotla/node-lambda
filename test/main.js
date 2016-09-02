c7c2bbb49bfa310ed0695e0ffded6840a3bcf142
Chase <chase@sillevis.net>
2016-09-02 10:59:38 +0200

    bump to 0.8.8 (#148)

--- test/main.js
+++ test/main.js
@@ -36,7 +36,7 @@ describe('node-lambda', function () {
   });
 
   it('version should be set', function () {
-    assert.equal(lambda.version, '0.8.7');
+    assert.equal(lambda.version, '0.8.8');
   });
 
   describe('_params', function () {
