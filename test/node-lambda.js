2f506a3b1293cdbb1e0606be5bc3fe562994f075
Tomoaki Abe <taneaki@gmail.com>
2017-05-18 14:32:08 +0900

    Feature simple callbackWaitsForEmptyEventLoop support (#284)
    * Fix to support `callbackWaitsForEmptyEventLoop`
    
    It is not a complete reproduction of AWS Lambda as it is a simple one.
    
    * Fix test as it supported `callbackWaitsForEmptyEventLoop`
    
    * Remove debugging code

--- test/node-lambda.js
+++ test/node-lambda.js
@@ -10,6 +10,20 @@ const nodeLambdaPath = path.join(__dirname, '..', 'bin', 'node-lambda');
 // The reason for specifying the node command in this test is to support Windows.
 describe('bin/node-lambda', () => {
   describe('node-lambda run', () => {
+    const _testMain = (expectedValues, done) => {
+      const run = spawn('node', [nodeLambdaPath, 'run', '--handler', '__test.handler']);
+      var stdoutString = '';
+      run.stdout.on('data', (data) => {
+        stdoutString += data.toString().replace(/\r|\n/g, '');
+      });
+
+      run.on('exit', (code) => {
+        assert.match(stdoutString, expectedValues.stdoutRegExp);
+        assert.equal(code, expectedValues.exitCode);
+        done();
+      });
+    };
+
     const _generateHandlerFile = (callbackString) => {
       fs.writeFileSync(
         '__test.js',
@@ -32,48 +46,70 @@ describe('bin/node-lambda', () => {
     });
 
     it('`node-lambda run` exitCode is `0` (callback(null))', (done) => {
-      const run = spawn('node', [nodeLambdaPath, 'run']);
-      var stdoutString = '';
-      run.stdout.on('data', (data) => {
-        stdoutString += data.toString().replace(/\r|\n/g, '');
-      });
-
-      run.on('exit', (code) => {
-        assert.match(stdoutString, /Success:$/);
-        assert.equal(code, 0);
-        done();
-      });
+      _generateHandlerFile('callback(null);');
+      _testMain({ stdoutRegExp: /Success:$/, exitCode: 0 }, done);
     });
 
     it('`node-lambda run` exitCode is `0` (callback(null, "text"))', (done) => {
       _generateHandlerFile('callback(null, "text");');
-
-      const run = spawn('node', [nodeLambdaPath, 'run', '--handler', '__test.handler']);
-      var stdoutString = '';
-      run.stdout.on('data', (data) => {
-        stdoutString += data.toString().replace(/\r|\n/g, '');
-      });
-
-      run.on('exit', (code) => {
-        assert.match(stdoutString, /Success:"text"$/);
-        assert.equal(code, 0);
-        done();
-      });
+      _testMain({ stdoutRegExp: /Success:"text"$/, exitCode: 0 }, done);
     });
 
     it('`node-lambda run` exitCode is `255` (callback(new Error("e")))', (done) => {
       _generateHandlerFile('callback(new Error("e"));');
+      _testMain({ stdoutRegExp: /Error: Error: e$/, exitCode: 255 }, done);
+    });
 
-      const run = spawn('node', [nodeLambdaPath, 'run', '--handler', '__test.handler']);
-      var stdoutString = '';
-      run.stdout.on('data', (data) => {
-        stdoutString += data.toString().replace(/\r|\n/g, '');
+    describe('node-lambda run (async)', function () {
+      this.timeout(5000); // give it time to setTimeout
+
+      const _generateHandlerFile = (callbackString, callbackWaitsForEmptyEventLoop) => {
+        const asyncCodeAndCallbackWaitsForEmptyEventLoopSettig = `
+          setTimeout(() => console.log('sleep 3500 msec'), 3500);
+          context.callbackWaitsForEmptyEventLoop = ${callbackWaitsForEmptyEventLoop};
+        `;
+        const testJsText = fs
+          .readFileSync('index.js').toString()
+          .replace(
+            /console.log\('Running index.handler'\);/,
+            asyncCodeAndCallbackWaitsForEmptyEventLoopSettig
+          )
+          .replace(/callback\(null\);/, callbackString);
+        fs.writeFileSync('__test.js', testJsText);
+      };
+
+      describe('callbackWaitsForEmptyEventLoop = true', () => {
+        it('`node-lambda run` exitCode is `0` (callback(null))', (done) => {
+          _generateHandlerFile('callback(null);', true);
+          _testMain({ stdoutRegExp: /Success:sleep 3500 msec$/, exitCode: 0 }, done);
+        });
+
+        it('`node-lambda run` exitCode is `0` (callback(null, "text"))', (done) => {
+          _generateHandlerFile('callback(null, "text");', true);
+          _testMain({ stdoutRegExp: /Success:"text"sleep 3500 msec$/, exitCode: 0 }, done);
+        });
+
+        it('`node-lambda run` exitCode is `255` (callback(new Error("e")))', (done) => {
+          _generateHandlerFile('callback(new Error("e"));', true);
+          _testMain({ stdoutRegExp: /Error: Error: esleep 3500 msec$/, exitCode: 255 }, done);
+        });
       });
 
-      run.on('exit', (code) => {
-        assert.match(stdoutString, /Error: Error: e$/);
-        assert.equal(code, 255);
-        done();
+      describe('callbackWaitsForEmptyEventLoop = false', () => {
+        it('`node-lambda run` exitCode is `0` (callback(null))', (done) => {
+          _generateHandlerFile('callback(null);', false);
+          _testMain({ stdoutRegExp: /Success:$/, exitCode: 0 }, done);
+        });
+
+        it('`node-lambda run` exitCode is `0` (callback(null, "text"))', (done) => {
+          _generateHandlerFile('callback(null, "text");', false);
+          _testMain({ stdoutRegExp: /Success:"text"$/, exitCode: 0 }, done);
+        });
+
+        it('`node-lambda run` exitCode is `255` (callback(new Error("e")))', (done) => {
+          _generateHandlerFile('callback(new Error("e"));', false);
+          _testMain({ stdoutRegExp: /Error: Error: e$/, exitCode: 255 }, done);
+        });
       });
     });
   });
