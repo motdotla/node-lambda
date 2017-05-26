7bc1df0259d4a1e0cf178e35a63f4834065c7617
abetomo <abe@enzou.tokyo>
2017-05-26 13:02:08 +0900

    Remove semicolon (Automatic update with `standard --fix`)
    Since JavaScript Standard Style does not add a semicolon,
    it has removed it.
    https://standardjs.com/rules.html#semicolons

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 // For development/testing purposes
 exports.handler = function (event, context, callback) {
-  console.log('Running index.handler');
-  console.log('==================================');
-  console.log('event', event);
-  console.log('==================================');
-  console.log('Stopping index.handler');
-  callback(null);
-};
+  console.log('Running index.handler')
+  console.log('==================================')
+  console.log('event', event)
+  console.log('==================================')
+  console.log('Stopping index.handler')
+  callback(null)
+}
--- test/handler/index.js
+++ test/handler/index.js
@@ -1,14 +1,14 @@
-'use strict';
+'use strict'
 
 exports.handler = (event, context, callback) => {
   // It changes to a boolean value with `!!`
   context.callbackWaitsForEmptyEventLoop =
-    !!event.callbackWaitsForEmptyEventLoop;
+    !!event.callbackWaitsForEmptyEventLoop
 
   if (event.asyncTest) {
-    setTimeout(() => console.log('sleep 3500 msec'), 3500);
+    setTimeout(() => console.log('sleep 3500 msec'), 3500)
   }
 
   /* eslint-disable no-eval */
-  eval(event.callbackCode);
-};
+  eval(event.callbackCode)
+}
