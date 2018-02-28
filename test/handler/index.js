0f45b24d9bfdc62baeeee84d487c61cc4545335b
Tomoaki Abe <abe@enzou.tokyo>
2018-03-01 01:21:18 +0900

    Feature AWS XRay SDK support (#414)
    * Add package to support AWSXRay
    
    * Upgrade packages
    
    * Fix AWSXRay support
    
    * Add code to test AWSXRay support

--- test/handler/index.js
+++ test/handler/index.js
@@ -1,5 +1,8 @@
 'use strict'
 
+// For testing AWSXRay support
+const AWSXRay = require('aws-xray-sdk-core')
+
 exports.handler = (event, context, callback) => {
   // It changes to a boolean value with `!!`
   context.callbackWaitsForEmptyEventLoop =
@@ -8,6 +11,13 @@ exports.handler = (event, context, callback) => {
   if (event.asyncTest) {
     setTimeout(() => console.log('sleep 3500 msec'), 3500)
   }
+
+  // https://docs.aws.amazon.com/xray/latest/devguide/scorekeep-lambda.html
+  // For testing AWSXRay support
+  AWSXRay.captureFunc('annotations', (subsegment) => {
+    subsegment.addAnnotation('Name', 'name')
+    subsegment.addAnnotation('UserID', 'piyo')
+  })
 
   /* eslint-disable no-eval */
   eval(event.callbackCode)
