f091fb49081b0768d1de0acabb04f62360ef3056
abetomo <abe@enzou.tokyo>
2021-03-30 09:34:14 +0900

    bump to 0.19.0

--- test/main.js
+++ test/main.js
@@ -149,7 +149,7 @@ describe('lib/main', function () {
   })
 
   it('version should be set', () => {
-    assert.equal(lambda.version, '0.18.0')
+    assert.equal(lambda.version, '0.19.0')
   })
 
   describe('_codeDirectory', () => {
