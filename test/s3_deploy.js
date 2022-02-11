d476d6c9ec578f1bb5239eed5efc803ae8061c42
Abe Tomoaki <abe@enzou.tokyo>
2022-02-11 23:18:00 +0900

    Follow best practice and require process rather than use global (#595)
    https://nodejs.org/dist/latest-v16.x/docs/api/process.html#process
    > The process object provides information about, and control over, the current Node.js process. While it is available as a global, it is recommended to explicitly access it via require or import:

--- test/s3_deploy.js
+++ test/s3_deploy.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const { assert } = require('chai')
+const process = require('process')
 const path = require('path')
 const aws = require('aws-sdk-mock')
 aws.setSDK(path.resolve('node_modules/aws-sdk'))
